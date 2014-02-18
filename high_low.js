70a5b12c4a7d0a27b1c4bfcd737b5100086251cf
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-18 12:16:18 -0500

    complete

--- high_low.js
+++ high_low.js
@@ -1,1 +1,15 @@
- 
+var rand_num = Math.ceil(10 * Math.random());
+
+var guess = prompt("Guess a number between 0 and 10:")
+while (guess != rand_num){
+  if ( guess < rand_num ){
+    console.log("Too low!");
+  } else {
+    console.log("Too high!")
+  }
+  var guess = prompt("Guess a number between 0 and 10:");
+}
+
+console.log("You are correct!");
+
+
