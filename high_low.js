77c61650ee5ecbf07b5d3a06c790064585dd5c89
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-18 12:18:13 -0500

    Add semi-colons

--- high_low.js
+++ high_low.js
@@ -1,11 +1,11 @@
 var rand_num = Math.ceil(10 * Math.random());
 
-var guess = prompt("Guess a number between 0 and 10:")
+var guess = prompt("Guess a number between 0 and 10:");
 while (guess != rand_num){
   if ( guess < rand_num ){
     console.log("Too low!");
   } else {
-    console.log("Too high!")
+    console.log("Too high!");
   }
   var guess = prompt("Guess a number between 0 and 10:");
 }
--- interest_calc.js
+++ interest_calc.js
@@ -2,6 +2,6 @@ var total_amount = prompt("How much did you borrowed totally?");
 var interest_rate = prompt("What's the interest rate?");
 var number_of_year = prompt("How long will you borrow?");
 
-loans = parseInt(total_amount) + parseInt(total_amount * interest_rate * number_of_year)
+loans = parseInt(total_amount) + parseInt(total_amount * interest_rate * number_of_year);
 
-console.log(loans)
+console.log(loans);
